3055d94d73483aa8d4c7021b63904ab55f3d5213
imashadkhan <ashad@gharmeshiksha.com>
2024-02-02 15:15:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Rishu" && b=="04021999")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
